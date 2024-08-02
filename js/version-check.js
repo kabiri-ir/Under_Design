a5416c6f28b0da1cce0fe71c49db9a882e23226d
kabiri-ir <kabiritdeveloper@gmail.com>
2024-08-02 09:49:38 +0330

    addcheckjs

---
+++ js/version-check.js
@@ -0,0 +1,16 @@
+(function() {
+    var lastVersioning = Date.UTC(2014, 11, 20, 2, 15, 10);
+    var lastCacheDateTime = localStorage.getItem('lastCacheDatetime');
+
+    if (lastCacheDateTime) {
+        if (lastVersioning > lastCacheDateTime) {
+            var reload = true;
+        }
+    }
+
+    localStorage.setItem('lastCacheDatetime', Date.now());
+
+    if (reload) {
+        location.reload(true);
+    }
+})();
